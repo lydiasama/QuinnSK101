3a8390230904f1cd7761b98d580b5e8c547347f9
lydiasama <hallelujah.lydia@gmail.com>
2018-10-13 00:49:00 +0700

    ui signup

--- public/login/login.js
+++ public/login/login.js
@@ -59,26 +59,7 @@ function signIn() {
 }
 
 function signUp() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    if (email.length < 4) {
-        alert('Please enter an email address.');
-        return;
-    }
-    if (password.length < 4) {
-        alert('Please enter a password.');
-        return;
-    }
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if (errorCode == '/auth/wrong-password') {
-            alert('Wrong password.');
-        } else {
-            alert(errorMessage);
-        }
-        console.log(error);
-    });
+   window.location = "../signup/signup.html";
 }
 
 function signOut() {
